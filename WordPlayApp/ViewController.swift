ccaddcfceef4ffdbf4d2ba4f57faa35a5628c868
Angelo <student@bluc02mf33cfh51.local>
2015-10-20 12:14:14 -0500

    Initial Commit

---
+++ WordPlayApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordPlayApp
+//
+//  Created by Student on 10/20/15.
+//  Copyright © 2015 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
